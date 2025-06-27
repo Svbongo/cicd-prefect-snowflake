c95925623c3c4d6195ee4290f0312acf9a98a052
Soham Vasudeo <sohamvasudeo@gmail.com>
2025-06-26 23:10:48 -0400

    Added more sample insert statements to test_data.sql

--- sql/test_data.sql
+++ sql/test_data.sql
@@ -5,3 +5,12 @@ INSERT INTO raw_data (id, data) VALUES
 (1, 10),
 (2, 25),
 (3, 40);
+
+INSERT INTO raw_data (id, data) VALUES
+(4, 55),
+(5, 65),
+(6, 70),
+(7, 80),
+(8, 95),
+(9, 110),
+(10, 130);
