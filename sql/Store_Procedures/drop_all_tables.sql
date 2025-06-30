efef59717fbda08a6da5bd06a46145747676fcd3
Soham Vasudeo <sohamvasudeo@gmail.com>
2025-06-30 19:58:38 -0400

    Added DDL and Store Procedure to drop all tables in the schema

---
+++ sql/Store_Procedures/drop_all_tables.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE PROCEDURE drop_all_tables()
+RETURNS STRING
+LANGUAGE SQL
+AS
+'
+DECLARE
+    cur CURSOR FOR
+        SELECT ''DROP TABLE IF EXISTS "'' || table_schema || ''"."'' || table_name || ''";''
+        FROM information_schema.tables
+        WHERE table_schema NOT IN (''INFORMATION_SCHEMA'', ''PUBLIC'')
+          AND table_type = ''BASE TABLE'';
+
+    sql_stmt STRING;
+BEGIN
+    FOR record IN cur DO
+        sql_stmt := record;
+        EXECUTE IMMEDIATE :sql_stmt;
+    END FOR;
+
+    RETURN ''✅ All user tables dropped.'';
+END;
+';
