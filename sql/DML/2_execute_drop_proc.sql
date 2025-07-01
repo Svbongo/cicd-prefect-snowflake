dc769c88834046aadb1d3274b4554402f477cb8e
Soham Vasudeo <sohamvasudeo@gmail.com>
2025-06-30 20:02:58 -0400

    Added DDL and Store Procedure to drop all tables in the schema

--- sql/DML/2_execute_drop_proc.sql
+++ sql/DML/2_execute_drop_proc.sql
@@ -1,1 +1,6 @@
-CALL drop_all_tables();
+-- Execute the stored procedure to drop all user tables
+-- Assumes the procedure has already been created in the target schema
+
+BEGIN
+    CALL drop_all_tables();
+END;
