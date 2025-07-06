68f7d8cfe860d10a65283c875f6f1e2ef52c1661
Soham Vasudeo <sohamvasudeo@gmail.com>
2025-07-06 18:15:04 -0400

    edited storeprocedure and yml flow

--- Snowflake/DEMO_DB/DATA_PIPELINE/Procedures/DROP_ALL_TABLES.sql
+++ Snowflake/DEMO_DB/DATA_PIPELINE/Procedures/DROP_ALL_TABLES.sql
@@ -1,21 +1,21 @@
-
 CREATE OR REPLACE PROCEDURE drop_all_tables()
 RETURNS STRING
 LANGUAGE SQL
 AS
-'
+$$
 DECLARE
     cur CURSOR FOR
-        SELECT ''DROP TABLE IF EXISTS "'' || table_schema || ''"."'' || table_name || ''";''
+        SELECT 'DROP TABLE IF EXISTS "' || table_schema || '"."' || table_name || '";' AS stmt
         FROM information_schema.tables
         WHERE table_schema = CURRENT_SCHEMA()
-          AND table_type = ''BASE TABLE'';
+          AND table_type = 'BASE TABLE';
     sql_stmt STRING;
 BEGIN
     FOR record IN cur DO
         sql_stmt := record.stmt;
         EXECUTE IMMEDIATE :sql_stmt;
     END FOR;
-    RETURN ''✅ All base tables dropped.'';
+
+    RETURN '✅ All base tables dropped successfully.';
 END;
-';
+$$;
